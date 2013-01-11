3476b4db43fb0ae3a0bac5e052c0f7cd8994e184
Jiyun Xie <pftxie@qq.com>
2013-01-11 14:04:07 +0800

    add client manager

---
+++ network-core/src/main/java/org/apache/niolex/network/client/ClientManager.java
@@ -0,0 +1,283 @@
+/**
+ * ClientManager.java
+ *
+ * Copyright 2013 The original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.niolex.network.client;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.niolex.commons.util.Runner;
+import org.apache.niolex.commons.util.SystemUtil;
+import org.apache.niolex.network.Config;
+import org.apache.niolex.network.IClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class manage client connection status, retry and fail over.
+ * User can set a list of server addresses in to this class, and it
+ * will pick up one randomly and try to connect to it.
+ * <p>
+ * User need to call {@link #connect()} manually to connect to server.
+ * If this method return true, the client is connected, otherwise
+ * it failed to connect to the first picked server, and we will try
+ * to connect to others in the background.
+ * <p>
+ * If user want to make sure finally connect to one server, please
+ * call {@link #retryConnect()}. See to document of that method for
+ * details.
+ *
+ * @author <a href="mailto:xiejiyun@gmail.com">Xie, Jiyun</a>
+ * @version 1.0.5
+ * @since 2013-1-11
+ */
+public class ClientManager<T extends IClient> {
+    private static final Logger LOG = LoggerFactory.getLogger(ClientManager.class);
+
+    /**
+     * The connection status of the managed Client.
+     *
+     * @author <a href="mailto:xiejiyun@gmail.com">Xie, Jiyun</a>
+     * @version 1.0.0, Date: 2012-6-2
+     */
+    public static enum Status {
+        INNITIAL, CONNECTING, CONNECTED, CLOSED
+    }
+
+    /**
+     * The time to sleep between retry.
+     */
+    private int sleepBetweenRetryTime = Config.RPC_SLEEP_BT_RETRY;
+
+    /**
+     * Times to retry get connected.
+     */
+    private int connectRetryTimes = Config.RPC_CONNECT_RETRY_TIMES;
+
+    /**
+     * The status of this Client.
+     */
+    private Status connStatus;
+
+    /**
+     * The latch to wait for this manager to finally get a connection.
+     */
+    private CountDownLatch connLatch;
+
+    /**
+     * The server address list
+     */
+    private List<InetSocketAddress> addressList;
+
+    /**
+     * The current using server address index
+     */
+    private int addressIndex;
+
+    /**
+     * The Client to be managed.
+     */
+    protected final IClient client;
+
+    /**
+     * Create a ClientManager to manage this client.
+     *
+     * @param client the client to be managed
+     */
+    public ClientManager(IClient client) {
+        super();
+        this.client = client;
+        this.connStatus = Status.INNITIAL;
+        this.connLatch = new CountDownLatch(1);
+    }
+
+    /**
+     * Try to connect to one server.
+     * We will only try once here, and if fail, we will start a background thread
+     * to try all others and return false.
+     *
+     * @return true if connected, false otherwise.
+     */
+    public boolean connect() {
+        if (addressList == null || addressList.size() == 0) {
+            throw new IllegalArgumentException("Please set addressList first!");
+        }
+        // If client already connected, we just return.
+        if (client.isWorking()) {
+            return true;
+        }
+        try {
+            doConnect();
+            return true;
+        } catch (IOException e) {
+            LOG.warn("Error occured when try to connect to {}.", client.getServerAddress(), e);
+            Runner.run(this, "retryConnect");
+        }
+        return false;
+    }
+
+    /**
+     * Try to re-connect to server.
+     * We will try at most {@link #connectRetryTimes} till we get a connection.
+     *
+     * @return true if connected, false otherwise
+     */
+    public boolean retryConnect() {
+        if (addressList == null || addressList.size() == 0) {
+            throw new IllegalArgumentException("Please set addressList first!");
+        }
+        // If client already connected, we just return.
+        if (client.isWorking()) {
+            return true;
+        }
+        this.connStatus = Status.CONNECTING;
+        if (connLatch.getCount() == 0) {
+            this.connLatch = new CountDownLatch(1);
+        }
+        for (int i = 0; i < connectRetryTimes; ++i) {
+            SystemUtil.sleep(sleepBetweenRetryTime);
+            LOG.info("RPC Client try to reconnect to server round {} ...", i);
+            try {
+                doConnect();
+                return true;
+            } catch (IOException e) {
+                // Not connected.
+                LOG.info("Try to re-connect to server failed. {}", e.toString());
+            }
+        }
+        this.connStatus = Status.CLOSED;
+        this.connLatch.countDown();
+        return false;
+    }
+
+    /**
+     * The internal method to do real connect.
+     *
+     * @throws IOException
+     */
+    private void doConnect() throws IOException {
+        addressIndex = (addressIndex + 1) % addressList.size();
+        client.setServerAddress(addressList.get(addressIndex));
+        client.connect();
+        this.connStatus = Status.CONNECTED;
+        connLatch.countDown();
+    }
+
+    /**
+     * Wait for we finally get a connection or been interrupted.
+     * <p>
+     * Notion! Please call {@link #connect()} or
+     * {@link #retryConnect()} first! We will not start a tread
+     * to try to connect to server in this method.
+     *
+     * @return true if connected, false if failed to connect to any server and we already
+     * stopped to retry.
+     * @throws InterruptedException if the current thread is interrupted while waiting
+     */
+    public boolean waitForConnected() throws InterruptedException {
+        this.connLatch.await();
+        return this.connStatus == Status.CONNECTED;
+    }
+
+    /**
+     * Stop the retry, close the client, and mark this client manager
+     * as closed.
+     * <p>
+     * We will set the {@link #connectRetryTimes} to 0 to stop the thread
+     * try to reconnect. So if any user want to reuse this client manager
+     * please re-set this field.
+     */
+    public void close() {
+        this.connectRetryTimes = 0;
+        this.client.stop();
+        this.connStatus = Status.CLOSED;
+    }
+
+    /**
+     * Get Connection Status of this client manager.
+     *
+     * @return current status
+     */
+    public Status getConnStatus() {
+        return connStatus;
+    }
+
+    /**
+     * Set the time in milliseconds that client with sleep between retry to connect
+     * to server.
+     *
+     * @param sleepBetweenRetryTime
+     */
+    public void setSleepBetweenRetryTime(int sleepBetweenRetryTime) {
+        this.sleepBetweenRetryTime = sleepBetweenRetryTime;
+    }
+
+    /**
+     * @return the current sleepBetweenRetryTime
+     */
+    public int getSleepBetweenRetryTime() {
+        return sleepBetweenRetryTime;
+    }
+
+    /**
+     * Set retry times.
+     *
+     * @param connectRetryTimes
+     */
+    public void setConnectRetryTimes(int connectRetryTimes) {
+        this.connectRetryTimes = connectRetryTimes;
+    }
+
+    /**
+     * @return the current connectRetryTimes
+     */
+    public int getConnectRetryTimes() {
+        return connectRetryTimes;
+    }
+
+    /**
+     * Set the server address list.
+     * We will shuffle it here in order to make it act more randomly.
+     *
+     * @param addressList
+     */
+    public void setAddressList(List<InetSocketAddress> addressList) {
+        Collections.shuffle(addressList);
+        this.addressList = addressList;
+    }
+
+    /**
+     * @return the current addressList
+     */
+    public List<InetSocketAddress> getAddressList() {
+        return addressList;
+    }
+
+    /**
+     * Set the socket connect timeout.
+     *
+     * @See {@link org.apache.niolex.network.IClient#setConnectTimeout(int)}
+     * @param timeout the new connect timeout
+     */
+    public void setConnectTimeout(int timeout) {
+        this.client.setConnectTimeout(timeout);
+    }
+
+}
---
+++ network-core/src/test/java/org/apache/niolex/network/client/ClientManagerTest.java
@@ -0,0 +1,227 @@
+/**
+ * ClientManagerTest.java
+ *
+ * Copyright 2013 The original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.niolex.network.client;
+
+import static org.junit.Assert.*;
+
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author <a href="mailto:xiejiyun@gmail.com">Xie, Jiyun</a>
+ * @version 1.0.5
+ * @since 2013-1-11
+ */
+public class ClientManagerTest {
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#ClientManager(org.apache.niolex.network.IClient)}.
+     * @throws InterruptedException
+     */
+    @Test
+    public void testClientManager() throws InterruptedException {
+        SocketClient sc = new SocketClient();
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        cm.setAddressList(Arrays.asList(new InetSocketAddress("www.baidu.com", 80)));
+        boolean b = cm.connect();
+        assertTrue(b);
+        assertEquals(cm.getConnStatus(), ClientManager.Status.CONNECTED);
+        assertTrue(cm.connect());
+        assertTrue(cm.retryConnect());
+        cm.close();
+        assertEquals(0, cm.getConnectRetryTimes());
+        b = cm.waitForConnected();
+        assertFalse(b);
+        cm.setConnectRetryTimes(2);
+        b = cm.retryConnect();
+        assertTrue(b);
+        b = cm.waitForConnected();
+        assertTrue(b);
+    }
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#connect()}.
+     */
+    @Test
+    public void testConnect() {
+        SocketClient sc = new SocketClient();
+        sc.setConnectTimeout(1000);
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        cm.setAddressList(Arrays.asList(new InetSocketAddress("127.0.0.1", 8090)));
+        boolean b = cm.connect();
+        assertFalse(b);
+        cm.close();
+        assertEquals(cm.getConnStatus(), ClientManager.Status.CLOSED);
+    }
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#retryConnect()}.
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void testRetryConnect() {
+        SocketClient sc = new SocketClient();
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        boolean b = cm.connect();
+        assertTrue(b);
+        assertTrue(cm.connect());
+    }
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#getConnStatus()}.
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void testGetConnStatus() {
+        SocketClient sc = new SocketClient();
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        List<InetSocketAddress> addressList = Collections.emptyList();
+        cm.setAddressList(addressList);
+        boolean b = cm.connect();
+        assertTrue(b);
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#setSleepBetweenRetryTime(int)}.
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void testSetSleepBetweenRetryTime() {
+        SocketClient sc = new SocketClient();
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        boolean b = cm.retryConnect();
+        assertTrue(b);
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#waitForConnected()}.
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void testWaitForConnected() {
+        SocketClient sc = new SocketClient();
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        List<InetSocketAddress> addressList = Collections.emptyList();
+        cm.setAddressList(addressList);
+        boolean b = cm.retryConnect();
+        assertTrue(b);
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#close()}.
+     */
+    @Test
+    public void testClose() {
+        SocketClient sc = new SocketClient();
+        sc.setConnectTimeout(1000);
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        cm.setAddressList(Arrays.asList(new InetSocketAddress("10.1.2.3", 8090)));
+        cm.setConnectRetryTimes(0);
+        cm.setSleepBetweenRetryTime(1);
+        boolean b = cm.retryConnect();
+        assertFalse(b);
+        assertEquals(cm.getConnStatus(), ClientManager.Status.CLOSED);
+    }
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#getSleepBetweenRetryTime()}.
+     * @throws InterruptedException
+     */
+    @Test
+    public void testGetSleepBetweenRetryTime() throws InterruptedException {
+        SocketClient sc = new SocketClient();
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        cm.setAddressList(Arrays.asList(new InetSocketAddress("www.baidu.com", 80),
+                new InetSocketAddress("127.0.0.1", 80)));
+        boolean b = cm.connect();
+        if (b) {
+            cm.close();
+            cm.setConnectRetryTimes(10);
+            b = cm.connect();
+        }
+        assertFalse(b);
+        Thread.sleep(10);
+        b = cm.waitForConnected();
+        assertTrue(b);
+    }
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#setConnectRetryTimes(int)}.
+     */
+    @Test
+    public void testSetConnectRetryTimes() {
+        SocketClient sc = new SocketClient();
+        sc.setConnectTimeout(1000);
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        cm.setAddressList(Arrays.asList(new InetSocketAddress("www.baidu.com", 80),
+                new InetSocketAddress("localhost", 80), new InetSocketAddress("localhost", 90)));
+        boolean b = cm.retryConnect();
+        assertTrue(b);
+        cm.close();
+    }
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#getConnectRetryTimes()}.
+     */
+    @Test
+    public void testGetConnectRetryTimes() {
+        SocketClient sc = new SocketClient();
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        cm.setConnectRetryTimes(100);
+        assertEquals(100, cm.getConnectRetryTimes());
+    }
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#setAddressList(java.util.List)}.
+     */
+    @Test
+    public void testSetAddressList() {
+        SocketClient sc = new SocketClient();
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        cm.setSleepBetweenRetryTime(1234);
+        assertEquals(1234, cm.getSleepBetweenRetryTime());
+    }
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#getAddressList()}.
+     */
+    @Test
+    public void testGetAddressList() {
+        SocketClient sc = new SocketClient();
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        cm.setAddressList(Arrays.asList(new InetSocketAddress("www.baidu.com", 80),
+                new InetSocketAddress("www.niolex.net", 80), new InetSocketAddress("www.niolex.org", 80)));
+        System.out.println(cm.getAddressList());
+    }
+
+    /**
+     * Test method for {@link org.apache.niolex.network.client.ClientManager#setConnectTimeout(int)}.
+     */
+    @Test
+    public void testSetConnectTimeout() {
+        SocketClient sc = new SocketClient();
+        ClientManager<SocketClient> cm = new ClientManager<SocketClient>(sc);
+        cm.setConnectTimeout(3100);
+        assertEquals(3100, sc.getConnectTimeout());
+    }
+
+}
