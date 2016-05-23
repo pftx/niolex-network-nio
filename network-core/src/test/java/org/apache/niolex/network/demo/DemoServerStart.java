a77abf266e0bed76b433d87c22c9a894279fdf99
Lex <pftxie@qq.com>
2016-05-23 14:33:35 +0800

    Add press test result.

---
+++ network-core/src/test/java/org/apache/niolex/network/demo/DemoServerStart.java
@@ -0,0 +1,11 @@
+package org.apache.niolex.network.demo;
+
+import java.io.IOException;
+
+public class DemoServerStart {
+
+    public static void main(String[] args) throws IOException {
+        DemoServer.main(args);
+    }
+
+}
